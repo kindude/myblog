232e2329d2cc7957243d185d629331b7d84f908b
Ubuntu <ubuntu@ip-172-31-60-56.ec2.internal>
2023-10-18 09:42:29 +0000

    adding routes and posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Posts</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+    <section>
+        <div>
+            <h1>My First Post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, voluptates dolorem ut saepe consequatur earum laborum tenetur eius voluptas dicta aperiam voluptatibus, at deleniti nam accusamus adipisci temporibus quod! Eum!</p>
+        </div>
+    </section>
+    <a href="/posts">Go back</a>
+</body>
+</html>
